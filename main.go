44146ab1aecb775511e009762c07a47e2b043b04
Ali Hashemi <60406325+haashemi@users.noreply.github.com>
2024-07-07 15:44:17 +0330

    feat: make changing the test cases easier

--- main.go
+++ main.go
@@ -20,67 +20,108 @@ func main() {
 	uniformImage := image.NewUniform(color.RGBA{127, 127, 127, 255})
 	// Size of the images we're going to experiment
 	imageRect := image.Rect(0, 0, 50, 50)
+
 	// Position of the vector over the image.
-	// Try these values:
-	// 	1. Basic: (0, 0) Should work fine
-	//  2. Negative: (-10, -10) Should panic on RGBA and Alpha
-	//  2. Negative Y: (0, -1) Should panic on RGBA and Alpha
-	//  3. Negative X: (-10, 2) Should *also* draw on right-side of the RGBA and Alpha images
-	//  4. Overflow: (35, 35) Should panic on RGBA and Alpha
-	//  4. Overflow Y: (0, 30) Should panic on RGBA and Alpha
-	//  5. Overflow X: (35, 0) Should *also* draw on left-side of the RGBA and Alpha images
-	//
-	// Feel free to try out more values.
-	addPoint := image.Pt(-10, 2)
-
-	// draw a basic rectangle just fow visualizing the issue.
-	createVector := func() *vector.Rasterizer {
-		vec := vector.NewRasterizer(25, 25)
-
-		vec.LineTo(float32(imageRect.Dx()), 0)
-		vec.LineTo(float32(imageRect.Dx()), float32(imageRect.Dy()))
-		vec.LineTo(0, float32(imageRect.Dy()))
-		vec.LineTo(0, 0)
-
-		return vec
+	// Contains three type of positions:
+	//  1. Common: Nothing special
+	//  2. Negative: Move the vector to top-left / top / left
+	//  3. Overflow: Move the vector to bottom-right / bottom / right
+	offsets := []struct {
+		Type  string
+		Point image.Point
+	}{
+		{"Common", image.Pt(0, 0)},       // Everything's fine
+		{"Negative", image.Pt(-10, -10)}, // Panic with image.RGBA and image.Alpha
+		{"NegativeY", image.Pt(0, -1)},   // Panic with image.RGBA and image.Alpha
+		{"NegativeX", image.Pt(-10, 2)},  // Draws  from the other-side of the RGBA and Alpha images
+		{"Overflow", image.Pt(35, 35)},   // Panic with image.RGBA and image.Alpha
+		{"OverflowY", image.Pt(0, 30)},   // Panic with image.RGBA and image.Alpha
+		{"OverflowX", image.Pt(35, 15)},  // Draws  from the other-side of the RGBA and Alpha images
 	}
 
-	// The goal of the issue is image.RGBA and image.Alpha, feel free to comment/uncomment any of these.
-	images := map[string]draw.Image{
-		// "Alpha": image.NewAlpha(imageRect), // PANICS!
-		"RGBA": image.NewRGBA(imageRect), // PANICS!
-		// "WrappedAlpha": ImageWrapper{image.NewAlpha(imageRect)}, // Works totally fine. But it's a hack.
-		// "WrappedRGBA":  ImageWrapper{image.NewRGBA(imageRect)},  // Works totally fine. But it's a hack.
-		// "Alpha16":      image.NewAlpha16(imageRect),             // Works totally fine.
-		// "CMYK":         image.NewCMYK(imageRect),                // Works totally fine.
-		// "Gray":         image.NewGray(imageRect),                // Works totally fine.
-		// "Gray16":       image.NewGray16(imageRect),              // Works totally fine.
-		// "NRGBA":        image.NewNRGBA(imageRect),               // Works totally fine.
-		// "NRGBA64":      image.NewNRGBA64(imageRect),             // Works totally fine.
-		// "RGBA64":       image.NewRGBA64(imageRect),              // Works totally fine.
-	}
+	// Generate the sample image with all offsets and images.
+	for _, offset := range offsets {
 
-	for imgType, img := range images {
-		// Just draw a background for experimental purposes.
-		// draw.Draw(img, img.Bounds(), image.White, image.Point{}, draw.Over)
+		// This experiment's targets are image.RGBA and image.Alpha.
+		// But feel free to try out any of them.
+		images := []struct {
+			Type  string
+			Image draw.Image
+		}{
+			// They both Panics on Negative, NegativeY, Overflow, and OverflowY.
+			// They also draw the skipped part of the vector higher/lower (depends)
+			// from the other side of the image on NegativeX and OverflowX.
+			{"Alpha", image.NewAlpha(imageRect)},
+			{"RGBA", image.NewRGBA(imageRect)},
 
-		vec := createVector()
+			// They work totally fine. But it's just for avoiding the real issue.
+			// Feel free to uncomment and test them out.
+			// {"WrappedAlpha", ImageWrapper{image.NewAlpha(imageRect)}},
+			// {"WrappedRGBA", ImageWrapper{image.NewRGBA(imageRect)}},
 
-		// If 'r' be greater than the vector's bounds, it'll panic.
-		// Thus we'll use its bounds for now to avoid unnecessary crashes.
-		vec.Draw(img, vec.Bounds().Add(addPoint), uniformImage, image.Point{})
-
-		f, err := os.OpenFile(fmt.Sprintf("out-%s.png", imgType), os.O_CREATE|os.O_WRONLY, os.ModePerm)
-		if err != nil {
-			fmt.Printf("[ERR] [IMG: %s] Failed to open a file > %v\n", imgType, err)
-			continue
+			// They all work totally fine.
+			{"NRGBA", image.NewNRGBA(imageRect)},
+			// {"NRGBA64", image.NewNRGBA64(imageRect)},
+			// {"RGBA64", image.NewRGBA64(imageRect)},
+			// {"Alpha16", image.NewAlpha16(imageRect)},
+			// {"CMYK", image.NewCMYK(imageRect)},
+			// {"Gray", image.NewGray(imageRect)},
+			// {"Gray16", image.NewGray16(imageRect)},
 		}
 
-		if err = png.Encode(f, img); err != nil {
-			fmt.Printf("[ERR] [IMG: %s] Failed to encode the image > %v\n", imgType, err)
-			continue
-		}
+		for _, img := range images {
+			func() {
+				defer func() {
+					if r := recover(); r != nil {
+						fmt.Printf("[FTL] [IMG: %s] [OFF: %s] Panic > %v \n", img.Type, offset.Type, r)
+					}
+				}()
 
-		fmt.Printf("[INF] [IMG: %s] Drawn and saved successfully\n", imgType)
+				vec := createVector(25, 25)
+
+				// If 'r' be greater than the vector's bounds, it'll panic.
+				// Thus we'll use its bounds for now to avoid unnecessary crashes.
+				//
+				// THIS IS WHERE EVERYTHING HAPPENS.
+				vec.Draw(img.Image, vec.Bounds().Add(offset.Point), uniformImage, image.Point{})
+
+				err := saveImage(img.Image, fmt.Sprintf("assets/%s-%s-current.png", offset.Type, img.Type))
+				if err != nil {
+					fmt.Printf("[ERR] [IMG: %s] [OFF: %s] Failed to save the image > %s\n", img.Type, offset.Type, err.Error())
+					return
+				}
+				fmt.Printf("[INF] [IMG: %s] [OFF: %s] Drawn and saved successfully\n", img.Type, offset.Type)
+			}()
+		}
 	}
+}
+
+// createVector create a basic rectangle vector just for visualizing the issue.
+//
+// It's NOT related to the issue.
+func createVector(w, h int) *vector.Rasterizer {
+	x, y := float32(w), float32(h)
+
+	vec := vector.NewRasterizer(w, h)
+	vec.LineTo(x, 0) // top right
+	vec.LineTo(x, y) // bottom right
+	vec.LineTo(0, y) // bottom left
+	vec.LineTo(0, 0) // top left
+	return vec
+}
+
+// saveImage encodes and write the img to path.
+//
+// It's NOT related to the issue.
+func saveImage(img image.Image, path string) error {
+	f, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY, os.ModePerm)
+	if err != nil {
+		return err
+	}
+
+	if err = png.Encode(f, img); err != nil {
+		return err
+	}
+
+	return nil
 }
